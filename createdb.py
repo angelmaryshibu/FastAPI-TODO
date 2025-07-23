be7365153ad954087b689c2b26334fb7cabf429c
angelmaryshibu <86075805+angelmaryshibu@users.noreply.github.com>
2025-07-23 11:17:58 +0530

    Update createdb.py

--- createdb.py
+++ createdb.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker ,declarative_base
 
-engine = create_engine("sqlite:///todo.db")
+engine = create_engine("postgresql+psycopg://postgres:angel123@localhost:5432/postgres")
 
 SessionLocal = sessionmaker(bind=engine,  autocommit=False)
 Base = declarative_base()
