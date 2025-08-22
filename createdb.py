9176baec91cf4e0ac8f925ee58ce0b30157e273a
Angel Mary Shibu <angelmaryash2@gmail.com>
2025-08-22 12:05:36 +0530

    Update database engine to use asyncpg for asynchronous operations

--- createdb.py
+++ createdb.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessionmaker
 from sqlalchemy.orm import sessionmaker ,declarative_base
 
-engine = create_async_engine("postgresql+psycopg://postgres:angel123@localhost:5432/postgres")
+engine = create_async_engine("postgresql+asyncpg://postgres:angel123@localhost:5432/postgres")
 
 SessionLocal = async_sessionmaker(bind=engine,  autocommit=False)
 Base = declarative_base()
